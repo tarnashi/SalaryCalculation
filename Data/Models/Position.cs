46d01aee7c54967946a8c442d0e32dd7507c3a31
tarnashi <tarnashi@yandex.ru>
2018-07-08 22:00:47 +0200

    init

---
+++ Data/Models/Position.cs
@@ -0,0 +1,11 @@
+﻿namespace Data.Models
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+
+
+    }
+}
---
+++ Data/Models/Worker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Models
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+
+        public int? SuperiorId { get; set; }
+        [ForeignKey("Id")]
+        public virtual Worker Superior { get; set; }
+        [ForeignKey("SuperiorId")]
+        public virtual List<Worker> Subordinates { get; set; }
+
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
