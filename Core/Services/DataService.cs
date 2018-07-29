5726ff42918990d12fcc3782535020bc17a2e008
tarnashi <tarnashi@yandex.ru>
2018-07-29 11:40:59 +0200

    Change Data project

--- Core/Services/DataService.cs
+++ Core/Services/DataService.cs
@@ -1,15 +1,23 @@
-﻿using Core.Abstract;
+﻿using System;
+using System.Linq;
+using Core.Abstract;
 using Data.DataAccess;
+using System.Collections.Generic;
 
 namespace Core.Services
 {
-    public class DataService : IDataService
+    public class DataService : IDataService, IDisposable
     {
         private readonly DataContext _ctx;
 
         public DataService()
         {
             _ctx = new DataContext();
+        }
+
+        public void Dispose()
+        {
+            _ctx?.Dispose();
         }
     }
 }
